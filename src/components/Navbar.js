64bb2141cbba1bdd29f3680a950f6be67c21088e
varad-n <hvi924926@gmail.com>
2025-03-26 22:48:02 +0530

    project in progress

--- src/components/Navbar.js
+++ src/components/Navbar.js
@@ -3,11 +3,25 @@ import Logo from "../Assets/Logo.svg"
 import { BsCart2 } from "react-icons/bs"
 import { HiOutlineBars3 } from "react-icons/hi2"
 import { Box, Drawer, ListItem, ListItemButton, ListItemText, ListItemIcon } from '@mui/material'
+import HomeIcon from "@mui/icons-material/Home"
+import InfoIcon from "@mui/icons-material/Info"
+import CommentRoundedIcon from "@mui/icon-material/CommentRounded"
+import PhoneRoundedIcon from "@mui/icons-material/PhoneRounded"
+import ShoppingCartRoundedIcon from "@mui/icons-material/ShoppingCartRounded"
+
 
 const Navbar = () => {
-  return (
-    <div>Navbar</div>
-  )
+
+    const [openMenu, setOpenMenu] = useState(false)
+    const menuOptions = [
+      {
+        text: "Home",
+        icon: <HomeIcon/>
+      },
+    ]
+
+    return <div>Navbar</div>
+  
 }
 
 export default Navbar
